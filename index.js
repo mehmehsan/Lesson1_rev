c1f3881c66320a8d20b05b18bc3dab1b66bd48b4
mehmehsan <66156650+mehmehsan@users.noreply.github.com>
2020-12-25 00:39:29 +0530

    Prime no. logic added

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 var btnPress = document.querySelector("#btn-pressed");
-var textInput = document.querySelector("#txtarea");
 var Outputtxt = document.querySelector("#output");
+var Datetxt = document.querySelector("#myDate");
+var option = document.querySelector("#selectBox");
 
 
 
 function eventHandler(){
-    Outputtxt.innerText = "jjdj" + textInput.value;
+    var day = Datetxt.value[8]+ Datetxt.value[9];
+    var year = Datetxt.value[0]+ Datetxt.value[1]+Datetxt.value[2]+ Datetxt.value[3];
+    
+    if(option.value=="leap"){
+
+    if(year%4==0) {
+        Outputtxt.innerText = Datetxt.value + " : Your year is a leap year ";
+    }
+    else {
+        Outputtxt.innerText = Datetxt.value + " : Your year is not a leap year ";
+    }
+    }
+
+    else{
+        Outputtxt.innerText = "Check prime no.";
+        var cnt = false;
+         for( var i=2; i <20 ; i++ ){
+             if (i!=day){
+                 if(day%i==0){                 
+                 cnt=true;
+                }
+                if(day==1){
+                    cnt=true;
+                }
+             }
+        
+         }
+         if(cnt==true) {
+             Outputtxt.innerText = Datetxt.value + " : is not prime number";
+         }
+         else{
+            Outputtxt.innerText = Datetxt.value + " : is a prime number";
+         }
+    }
+
     
 }
 
